971c835c646135ed3046c5567c116faf92fbf643
alesin9x <a-alesin@nmt-9.com>
2024-08-26 22:27:52 +0400

    Первая страница

---
+++ views/parameter/index.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\grid\GridView;
+use yii\helpers\Html;
+
+$this->title = 'Parameters';
+?>
+
+<main>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-2">
+                <?= Html::a('Создать параметер', ['create'], ['class' => 'btn btn-link']) ?>
+            </div>
+        </div>
+        <?=
+        GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $queryParameter,
+            'columns' => [
+                'ID',
+                'title',
+                [
+                    'attribute' => 'icon',
+                    'format' => 'html',
+                    'value' => function ($model) {
+                        return $model->icon ? Html::img($model->getIconUrl(), ['width' => '50']) : null;
+                    }
+                ],
+                'icon_original_name',
+                [
+                    'attribute' => 'icon_gray',
+                    'format' => 'html',
+                    'value' => function ($model) {
+                        return $model->icon_gray ? Html::img($model->getIconGrayUrl(), ['width' => '50']) : null;
+                    }
+                ],
+                'icon_gray_original_name'
+            ]
+        ])
+        ?>
+    </div>
+</main>
